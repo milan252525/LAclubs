3f6b60d82131e579304eb7ac83f33112b30552ae
milan252525 <milan.abraham2@seznam.cz>
2020-11-05 18:44:33 +0100

    added ssl cert

--- run_server.py
+++ run_server.py
@@ -1,5 +1,5 @@
 from gevent.pywsgi import WSGIServer
 from app import app
 
-http_server = WSGIServer(('', 443), app)
+http_server = WSGIServer(('', 443), app, cert='key.pem')
 http_server.serve_forever()
