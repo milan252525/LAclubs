e7354c10efde941a7399b63db87b8df2dccbc642
milan252525 <milan.abraham2@seznam.cz>
2020-11-06 10:34:08 +0000

    force https usage

--- cmd.sh
+++ cmd.sh
@@ -1,2 +1,4 @@
-sudo venv/bin/gunicorn -w 4 -b 139.59.139.93:443 --certfile cert.pem --keyfile key.pem --log-level debug --timeout 90 app:app
+sudo venv/bin/gunicorn -w 4 -b 0.0.0.0:443 --certfile cert.pem --keyfile key.pem --log-level debug --timeout 90 app:app &
+sudo venv/bin/gunicorn -w 2 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
+fg
 
