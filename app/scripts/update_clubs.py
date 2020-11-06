7af288d0dfc26cf431dde1bf63493c54ab0d75f1
milan_25 <milan.abraham2@seznam.cz>
2020-11-06 11:37:43 +0100

    fixed errors when game in maintenance

--- app/scripts/update_clubs.py
+++ app/scripts/update_clubs.py
@@ -1,10 +1,15 @@
 import time
 import brawlstats
 import json
+import sys
 
 token="eyJ0eXAiOiJKV1QiLCJhbGciOiJIUzUxMiIsImtpZCI6IjI4YTMxOGY3LTAwMDAtYTFlYi03ZmExLTJjNzQzM2M2Y2NhNSJ9.eyJpc3MiOiJzdXBlcmNlbGwiLCJhdWQiOiJzdXBlcmNlbGw6Z2FtZWFwaSIsImp0aSI6ImFhOTVkZjBjLTNiYmYtNDkzNC05MWFlLWMxYTFkZDA3ZjQ5OSIsImlhdCI6MTYwNDU3MDc3NSwic3ViIjoiZGV2ZWxvcGVyLzFiNTkzZmFlLWRlNWItZDUwOS0zODI5LTg3OTg3YzZjOWI0ZiIsInNjb3BlcyI6WyJicmF3bHN0YXJzIl0sImxpbWl0cyI6W3sidGllciI6ImRldmVsb3Blci9zaWx2ZXIiLCJ0eXBlIjoidGhyb3R0bGluZyJ9LHsiY2lkcnMiOlsiMTg1LjcxLjIzMi4xOTQiLCIxMzkuNTkuMTM5LjkzIl0sInR5cGUiOiJjbGllbnQifV19.cDQGLc2t0V6AGVAo0KOW6CbKXffb856grnZjRvuS2qoDeRUwEKMUO1bvSBt7ZMwUjKsKeWW-HanEfGNTFp5btA"
 
-api = brawlstats.Client(token)
+try:
+    api = brawlstats.Client(token)
+except brawlstats.errors.ServerError:
+    print("api server down")
+    sys.exit()
 
 with open("../data/club_data.json", "r") as output_file:
     output = json.load(output_file)
@@ -20,7 +25,7 @@ for club in clubs:
     except brawlstats.errors.RequestError as e:
         print(e)
         print("API OFFLINE STOPPING...")
-        break
+        sys.exit()
 
     club_data = {
         'name': data['name'],
