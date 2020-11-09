1fb94c163ab1dafc7ec1aae732f4d9fe36dd90c9
milan252525 <milan.abraham2@seznam.cz>
2020-11-09 16:59:48 +0100

    moved http work to https

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
-sudo venv/bin/gunicorn -w 4 -b 0.0.0.0:443 --certfile cert.pem --keyfile key.pem --log-level debug --timeout 90 app:app &
-sudo venv/bin/gunicorn -w 2 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
+sudo venv/bin/gunicorn -w 5 -b 0.0.0.0:443 --certfile cert.pem --keyfile key.pem --log-level debug --timeout 90 app:app &
+sudo venv/bin/gunicorn -w 1 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
 fg
 
