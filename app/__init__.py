c7d4e57c64f1005748aecb640a1749c89a9da6d9
milan252525 <milan.abraham2@seznam.cz>
2021-01-03 17:36:26 +0100

    discord oauth test

--- app/__init__.py
+++ app/__init__.py
@@ -1,9 +1,18 @@
 from flask import Flask
 from flask_talisman import Talisman
 from flask_pymongo import PyMongo
+from flask_discord import DiscordOAuth2Session
 
 app = Flask(__name__)
+
 app.config["MONGO_URI"] = "MONGO/laclubs"
+app.config["DISCORD_CLIENT_ID"] = 795325486242857000    # Discord client ID.
+app.config["DISCORD_CLIENT_SECRET"] = "SECRET"                # Discord client secret.
+app.config["DISCORD_REDIRECT_URI"] = ""                 # URL to your callback endpoint.
+app.config["DISCORD_BOT_TOKEN"] = ""                    # Required to access BOT resources.
+
+discord = DiscordOAuth2Session(app)
+
 mongo = PyMongo(app)
 
 csp = {
--- app/routes.py
+++ app/routes.py
@@ -1,5 +1,6 @@
-from flask import render_template, request, send_from_directory, make_response
-from app import app
+from flask import render_template, request, send_from_directory, make_response, redirect, url_for
+from app import app, discord
+from flask_discord import requires_authorization, Unauthorized
 from . import get_data
 import os
 
@@ -68,3 +69,30 @@ def add_header(resp):
     resp.headers['X-Frame-Options'] = 'SAMEORIGIN'
     resp.headers['X-XSS-Protection'] = '1; mode=block'
     return resp
+
+@app.route("/login/")
+def login():
+    return discord.create_session()
+
+@app.route("/me/")
+@requires_authorization
+def me():
+    user = discord.fetch_user()
+    return f"""
+    <html>
+        <head>
+            <title>{user.name}</title>
+        </head>
+        <body>
+            <img src='{user.avatar_url}' />
+        </body>
+    </html>"""
+
+@app.route("/callback/")
+def callback():
+    discord.callback()
+    return redirect(url_for(".me"))
+
+@app.errorhandler(Unauthorized)
+def redirect_unauthorized(e):
+    return redirect(url_for("login"))
