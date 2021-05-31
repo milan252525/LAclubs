baa06018b5818b4af0a69975c628b72afd2020cf
milan_25 <milan.abraham2@seznam.cz>
2021-05-31 13:18:28 +0200

    new certificate location

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
-venv/bin/gunicorn -w 3 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/milan25.com/cert.pem --keyfile /etc/letsencrypt/live/milan25.com/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
+venv/bin/gunicorn -w 3 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/laclubs.net/cert.pem --keyfile /etc/letsencrypt/live/laclubs.net/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
 venv/bin/gunicorn -w 1 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
 fg
 
