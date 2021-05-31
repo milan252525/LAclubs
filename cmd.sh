a75adc43496312c5b25dce64628e6af9ab023c0b
milan_25 <milan.abraham2@seznam.cz>
2021-05-31 12:39:12 +0200

    no need for root

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,4 @@
-sudo venv/bin/gunicorn -w 3 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/milan25.com/cert.pem --keyfile /etc/letsencrypt/live/milan25.com/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
-sudo venv/bin/gunicorn -w 1 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
+venv/bin/gunicorn -w 3 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/milan25.com/cert.pem --keyfile /etc/letsencrypt/live/milan25.com/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
+venv/bin/gunicorn -w 1 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
 fg
 
