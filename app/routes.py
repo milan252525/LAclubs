d58302e67b06be54344e89154d8acdf499d0ae12
milan_25 <milan.abraham2@seznam.cz>
2021-06-01 14:52:03 +0200

    swapped name with value

--- app/routes.py
+++ app/routes.py
@@ -163,12 +163,12 @@ def add_header(resp):
     return resp
 
 class Regions(enum.Enum):
-    EU = "Europe"
-    AS = "Asia"
-    NA = "North America"
-    LATAM = "Latin America"
-    ME = "Middle East"
-    AUS = "Australia"
+    Europe = "EU"
+    Asia = "AS"
+    North_America = "NA"
+    Latin_America = "LATAM"
+    Middle_East = "ME"
+    Australia = "AU"
 
 @discord.command(annotations={"region": "Choose region"})
 def clubs(ctx, region: Regions):
