24848a1804c63dc254314843d9606432bdf1d7ef
milan_25 <milan.abraham2@seznam.cz>
2021-11-12 19:02:30 +0100

    fixed update script

--- app/scripts/db_update_clubs.py
+++ app/scripts/db_update_clubs.py
@@ -32,6 +32,8 @@ clubs = db['tracked_clubs'].find()
 time_data = time.gmtime(int(time.time()))
 save_history = time_data.tm_hour % 4 == 0 and time_data.tm_min < 5
 
+errors = 0
+
 for club in clubs:
     tag = club['tag']
     print(f"Updating {club['name']}")
@@ -39,9 +41,11 @@ for club in clubs:
         data = api.get_club(tag)
         data = data.raw_data
     except brawlstats.errors.RequestError as e:
-        print(e)
-        print("API OFFLINE STOPPING...")
-        sys.exit()
+        print(club, ['name']e)
+        errors += 1
+        if error >= 3:
+            print("API OFFLINE STOPPING...")
+            sys.exit()
     try:
         pres, vp_count, sen_count = get_pres_vp_sen(data['members'])
         club_data = {
