957126717983e25af7844f387be7f825d47818cf
milan_25 <milan.abraham2@seznam.cz>
2022-05-11 16:22:51 +0200

    decreased amount of workers

--- cmd.sh
+++ cmd.sh
@@ -1,4 +1,3 @@
-venv/bin/gunicorn -w 3 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/laclubs.net/fullchain.pem --keyfile /etc/letsencrypt/live/laclubs.net/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
+venv/bin/gunicorn -w 1 -b 0.0.0.0:443 --certfile /etc/letsencrypt/live/laclubs.net/fullchain.pem --keyfile /etc/letsencrypt/live/laclubs.net/privkey.pem --log-level debug --timeout 90 -k "gevent" app:app &
 venv/bin/gunicorn -w 1 -b 0.0.0.0:80 --log-level debug --timeout 90 app:app &&
 fg
-
